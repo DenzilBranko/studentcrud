52e40e3b0f9b466ac96f052c4005c6afbb3f641f
DenzilBranko <denzilbranko@gmail.com>
2018-07-05 00:20:58 +0530

    second commit

--- collegeweb/students/views.py
+++ collegeweb/students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import StudentProfile
 
 
@@ -12,6 +14,21 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     pass
+
+class saveData(CreateView):
+    model = StudentProfile
+    fields = ['fname','lname','email','address','password','mobile']
+
+class editData(UpdateView):
+    model = StudentProfile
+    fields = ['fname','lname','email','address','password','mobile']
+
+class deleteData(DeleteView):
+    model = StudentProfile
+    success_url = reverse_lazy('students:index')
+
+
+
 
 
 
